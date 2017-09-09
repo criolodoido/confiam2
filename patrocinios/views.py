02dcdbdb797841a7347a0212581cf504a3e3ab12
criolodoido <victor.santos.cd@gmail.com>
2017-09-09 00:15:30 -0300

    corrigindo

--- patrocinios/views.py
+++ patrocinios/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
-from django.utils import timezone
 from .models import Patrocinios
+from django.utils import timezone
+
 
 def patrocinios(request):
 	posts = Patrocinios.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
